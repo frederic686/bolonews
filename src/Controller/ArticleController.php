dcf808c4e9059df045cf8f4d237239bc2e6d6155
fred686 <frederic.maripalane@hotmail.fr>
2025-08-02 12:49:19 +0200

    pages articles en cours

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $articlesPublies = $entityManager->getRepository(Article::class)->findBy(
+            ['publie' => true],
+            ['dateCreation' => 'DESC']
+        );
+
         return $this->render('article/articles.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articlesPublies,
         ]);
     }
 }
